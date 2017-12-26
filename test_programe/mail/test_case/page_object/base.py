2c23a14709f60cd935b5882262d16fcbdb816920
Administrator <2513953126@qq.com>
2017-12-26 11:05:52 +0800

    Python2

---
+++ test_programe/mail/test_case/page_object/base.py
@@ -0,0 +1,44 @@
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+import unittest
+
+
+# 基本层
+class Base(object):
+    # def __init__(self, driver, base_url='https://t.pangpangpig.com'):
+    def __init__(self, driver, base_url='http://mail.163.com'):
+        self.driver = driver
+        self.base_url = base_url
+        self.timeout = 30
+
+    def _open(self, url):
+        url_ = self.base_url + url
+        # print(url_)
+        self.driver.maximize_window()
+        self.driver.get(url_)
+        sleep(2)
+        # sleep(5)
+        assert self.driver.current_url == url_, 'Did ont land on %s' % url_
+
+    def open(self):
+        self._open(self.url)
+
+    # *参数个数不是固定的（By.ID, 'kw'）
+    def find_element(self, *loc):
+        return self.driver.find_element(*loc)
+        # print "*loc"
+
+    def iframe(self, iframeid):
+        return self.driver.switch_to.frame(iframeid)
+
+    def iframe_out(self):
+        return self.driver.switch_to.default_content()
+
+
+
+# 用于验证该脚本是否有效
+if __name__ == "__main__":
+    unittest.main()
